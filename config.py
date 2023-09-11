0eb75bb76c71b1b36297560f3693351c48de9c9a
wfildef <144725226+wfildef@users.noreply.github.com>
2023-09-11 21:30:55 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+#чат проекта
+chatLink = "https://t.me/test4"
+
+#канал с выплатами
+paymentsLink ="https://t.me/test0"
+
+#профиль администратора
+adminLink ="https://t.me/test1"
+
+#профиль разработчика
+developerLink ="https://t.me/test2"
+
+#награда за одного приглашенного юзера
+reward = 1
+
+#токен бота
+botToken = ""
+
+#TG ID администратора
+adminID = 99999999999
+
+#Юзернейм бота (используется для создания реф ссылки)
+botName = "test_bot"
